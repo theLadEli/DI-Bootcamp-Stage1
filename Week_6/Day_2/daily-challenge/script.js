2ca5336c5bdcaea06069135db632a99e27269128
Eli <coheneli98@gmail.com>
2024-12-09 20:38:27 +0200

    Finished W6D2 daily challenge

--- Week_6/Day_2/daily-challenge/script.js
+++ Week_6/Day_2/daily-challenge/script.js
@@ -1,15 +1,37 @@
+let formattedGifs = {}
+
 async function getRelevantGif(event) {
     event.preventDefault()
-    const category = document.getElementById("category").value
+    const category = $("#category").val()
 
     const result = await fetch(`https://api.giphy.com/v1/gifs/random?tag=${category}&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`)
 
     const data = await result.json()
     const gifURL = data.data.images.original.url
-    console.log(gifURL);
-    
+    console.log(`Gif URL: ${gifURL}`)
+    const gifID = data.data.id
+    console.log(`Gif ID: ${gifID}`)
 
-    document.querySelector("img").src = gifURL
+    console.log(formattedGifs)
+    formattedGifs[gifID] = `<div id="${gifID}"><img src="${gifURL}"><button onclick="deleteGif('${gifID}')">Delete</button></div>`
+    console.log(formattedGifs)
+
+    regenerateGifGrid()
 }
 
-document.querySelector("form").addEventListener("submit", getRelevantGif)
+function deleteGif(gifID){
+    delete formattedGifs[gifID]
+    regenerateGifGrid()
+}
+
+function deleteAllGifs() {
+    formattedGifs = {}
+    regenerateGifGrid()
+}
+
+function regenerateGifGrid() {
+    const gridHTML = Object.values(formattedGifs).join("");
+    $("#grid-of-gifs").html(gridHTML);
+}
+
+$("form").on("submit", getRelevantGif)
